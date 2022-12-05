3fc1cf75aa89ee5f5692c012baa9da535cfead5e
Paolo224 <picariellopaolo224@gmail.com>
2022-12-05 12:49:04 +0100

    Aggiunta della media allo snack-3

--- snack-3.js
+++ snack-3.js
@@ -1,61 +1,68 @@
 const zucchina = [
     {
         'varietà' : 'veneta',
-        'peso' : '500 g',
+        'peso' : 500,
         'lunghezza' : 12,
     },
     {
         'varietà' : 'piemontese',
-        'peso' : '1,5 kg',
+        'peso' : 100,
         'lunghezza' : 24,
     },
     {
         'varietà' : 'sarda',
-        'peso' : '2 kg',
+        'peso' : 200,
         'lunghezza' : 7,
     },
     {
         'varietà' : 'campana',
-        'peso' : '1 kg',
+        'peso' : 100,
         'lunghezza' : 11,
     },
     {
         'varietà' : 'calabrese',
-        'peso' : '400 g',
+        'peso' : 400,
         'lunghezza' : 12,
     },
     {
         'varietà' : 'pugliese',
-        'peso' : '5 kg',
+        'peso' : 500,
         'lunghezza' : 24,
     },
     {
         'varietà' : 'abbruzzese',
-        'peso' : '2 kg',
+        'peso' : 200,
         'lunghezza' : 7,
     },
     {
         'varietà' : 'veneta',
-        'peso' : '1 kg',
+        'peso' : 100,
         'lunghezza' : 12,
     },
     {
         'varietà' : 'sarda',
-        'peso' : '500 g',
+        'peso' : 500,
         'lunghezza' : 7,
     },
     {
         'varietà' : 'campana',
-        'peso' : '2 kg',
+        'peso' : 200,
         'lunghezza' : 24,
     }
 ];
 
-let somma = 0;
+let sommaLunghezza = 0;
+let sommaPeso = 0;
+let mediaLunghezza = 0;
+let mediaPeso = 0;
 
 for (let i = 0; i < zucchina.length; i++){
     const zucchinaCorrente = zucchina[i];
-    somma += zucchinaCorrente.lunghezza
+    sommaLunghezza += zucchinaCorrente.lunghezza;
+    mediaLunghezza = sommaLunghezza / zucchina.length;
+    sommaPeso += zucchinaCorrente.peso;
+    mediaPeso = sommaPeso / zucchina.length;
 }
 
-console.log('La somma del peso delle zucchine è di: ' + somma + ' centimetri');
+console.log('La somma del peso delle zucchine è di: ' + sommaLunghezza + ' centimetri');
+console.log('La media della lunghezza e del peso: ' + mediaLunghezza + ' ' + mediaPeso);
